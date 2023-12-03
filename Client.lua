fa00750722264b8b2619f5be8425faa6ea7861fc
Vreum <84644035+vrE-Um@users.noreply.github.com>
2023-12-03 18:35:14 -0300

    Create Client.lua

---
+++ Client.lua
@@ -0,0 +1,20 @@
+local CHS: ChangeHistoryService = game:GetService('ChangeHistoryService');
+
+local Toolbar: PluginToolbar = plugin:CreateToolbar('');
+
+
+
+return function(Title: string, Description: string, Icon: string, RecordName: string, CWVH: boolean, Method: () -> (), Arguments: { }?): ()
+        local ToolbarButton: PluginToolbarButton = Toolbar:CreateButton(Title, Description, Icon);
+        ToolbarButton.ClickableWhenViewportHidden = CWVH; 
+
+        local function ButtonMethod(): any?
+                pcall(function(): ()
+                        return Arguments and Method(table.unpack(Arguments)) or Method();
+                end);
+
+                CHS:SetWaypoint(RecordName);
+        end
+
+        return (ToolbarButton.Click:Connect(ButtonMethod));
+end
